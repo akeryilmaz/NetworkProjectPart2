7f9ead7fba5854b504e5ac22e23c5d41d75151aa
e2167369 <e2167369@s.projectp2.ch-geni-net.instageni.nps.edu>
2019-12-26 08:22:24 -0800

    configs for all experiments added

---
+++ r3/config38.sh
@@ -0,0 +1,6 @@
+tc qdisc del dev eth1 root
+tc qdisc del dev eth2 root
+tc qdisc del dev eth3 root
+tc qdisc add dev eth1 root netem loss 38% delay 3ms
+tc qdisc add dev eth2 root netem loss 38% delay 3ms
+tc qdisc add dev eth3 root netem loss 38% delay 3ms
